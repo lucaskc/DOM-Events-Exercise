37d597bfa7c974667481ec66e7c45e3520a7353b
Lucas Kassouf Crocomo <lucas.crocomo@gmail.com>
2018-09-10 16:24:59 -0300

    adding my solution

---
+++ script.js
@@ -0,0 +1,49 @@
+const button = document.getElementById("btn1");
+const input = document.getElementById("ipt1");
+const ul = document.querySelector("ul");
+const li = document.querySelectorAll("li");
+
+
+const removeParent = (event) => {
+	event.target.removeEventListener("click", removeParent);
+	event.target.parentNode.remove();
+}
+
+const addDeleteButton = (item) => {
+	let btn = document.createElement("button");
+	btn.appendChild(document.createTextNode("Delete"));
+	btn.onclick = removeParent;
+	item.appendChild(btn);
+}
+
+const createListElement = () => {
+	const li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	addDeleteButton(li)
+	ul.appendChild(li);
+	input.value = "";
+}
+
+const addItemOnClick = () => {
+	if (input.value.length > 0) {
+		createListElement();
+	}
+}
+
+const addItemOnKeypress = (event) => {
+	if (input.value.length > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+const toggleCheck = (event) => {
+	if (event.target.tagName === "li") {
+		event.target.classList.toggle("done");
+	}
+}
+
+
+li.forEach(addDeleteButton);
+ul.addEventListener("click", toggleCheck);
+button.addEventListener("click", addItemOnClick);
+input.addEventListener("keypress", addItemOnKeypress);
